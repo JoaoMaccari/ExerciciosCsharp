a835e2aa419dd19f6ca66e412cf5a3eee45c34dd
João <joaomaccari@hotmail.com>
2022-11-03 10:43:04 -0300

    implementação form.2

--- Ex03/Ex03/Form1.Designer.cs
+++ Ex03/Ex03/Form1.Designer.cs
@@ -44,7 +44,7 @@ namespace Ex03 {
             this.label1.Size = new System.Drawing.Size(161, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sistema Login";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            
             // 
             // label2
             // 
